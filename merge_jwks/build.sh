1f6d789df71fcdff5d311e3de3eef0083dff5ca9
Ahmet Soormally <ahmet@mangomm.co.uk>
2020-05-04 23:00:21 +0100

    fix pem encoding

--- merge_jwks/build.sh
+++ merge_jwks/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t mangomm/merge-jwks:0.1 --build-arg version="feature/merge-jwks" .
+docker build -t mangomm/merge-jwks:0.2 --build-arg version="feature/merge-jwks" .
 #docker push mangomm/merge-jwks:0.1
