591e89af3e3026bac8959bd29cfb3cd5fdb3a3ed
Ahmet Soormally <ahmet@mangomm.co.uk>
2020-05-08 08:12:09 +0100

    clearning up the build

--- merge_jwks/build.sh
+++ merge_jwks/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-docker build --no-cache -t mangomm/merge-jwks:0.5 --build-arg version="merge-jwks-0.5" .
-docker push mangomm/merge-jwks:0.5
+export VERSION=merge-jwks-0.6
+echo $VERSION
+
+git tag ${VERSION}
+git push -f --tags
+docker build --no-cache -t mangomm/${VERSION} --build-arg version=${VERSION} .
+docker push mangomm/${VERSION}
