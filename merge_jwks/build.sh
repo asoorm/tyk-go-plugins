5d7f786b38a6db222af486bdadc9e5e3db519438
Ahmet Soormally <ahmet@mangomm.co.uk>
2020-05-08 08:32:00 +0100

    fixing dockerfile

--- merge_jwks/build.sh
+++ merge_jwks/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export VERSION=merge-jwks-0.6
+export VERSION=merge-jwks-0.5
 echo $VERSION
 
 git tag ${VERSION}
 git push -f --tags
 docker build --no-cache -t mangomm/${VERSION} --build-arg version=${VERSION} .
-docker push mangomm/${VERSION}
+#docker push mangomm/${VERSION}
